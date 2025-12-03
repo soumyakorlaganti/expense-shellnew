2588c6930afa008fb5af60b466f51659bddc5f2a
soumya <korlagani.soumya@gmail.com>
2025-12-03 13:12:08 +0530

    expense

--- mysql.sh
+++ mysql.sh
@@ -21,13 +21,13 @@ VALIDATE(){
     fi
 }
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1
-else
-    echo "You are super user."
-fi
+# if [ $USERID -ne 0 ]
+# then
+#     echo "Please run this script with root access."
+#     exit 1
+# else
+#     echo "You are super user."
+# fi
 
 dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Server"
