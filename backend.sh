815e02e0ba8328e83457c0024280c198ffc12b58
soumya <korlagani.soumya@gmail.com>
2025-12-03 18:18:07 +0530

    functions

--- backend.sh
+++ backend.sh
@@ -83,7 +83,7 @@ VALIDATE $? "Enabling backend"
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h db.soumyadevops.space -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+mysql -h db.soumyadevops.space -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOG_FILE
 VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOG_FILE
