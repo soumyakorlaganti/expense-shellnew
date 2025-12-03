1bc6b648559a02d87315e8670f40879853129ae0
soumya <korlagani.soumya@gmail.com>
2025-12-03 14:38:54 +0530

    expense

--- mysql.sh
+++ mysql.sh
@@ -1,55 +1,114 @@
+# #!/bin/bash
+
+# USERID=$(id -u)
+# TIMESTAMP=$(date +%F-%H-%M-%S)
+# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+# LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+# echo "please enter root password:"
+# read -s mysql_root_password
+
+# if [ $USERID -ne 0 ]
+# then
+#     echo "Please run this script with root access."
+#     exit 1
+# else
+#     echo "You are super user."
+# fi
+
+
+# VALIDATE(){
+#     if [ $1 -ne 0 ]
+#     then 
+#         echo -e "$2...$R FAILURE $N"
+#         exit 1
+#     else
+#         echo -e "$2...$G SUCCESS $N"
+#     fi
+# }
+
+
+# dnf install mysql-server -y &>>$LOGFILE
+# VALIDATE $? "Installing MySQL Server"
+
+# systemctl enable mysqld &>>$LOGFILE
+# VALIDATE $? "Enabling MySQL Server"
+
+# systemctl start mysqld &>>$LOGFILE
+# VALIDATE $? "Starting MySQL Server"
+
+# # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# # VALIDATE $? "Setting  mysql root password"
+
+# # Below code will be useful for idempotent nature
+# mysql -h db.soumyadevops.space -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+# if [ $? -ne 0 ]
+# then
+#     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+#     VALIDATE $? "MySQL Root password Setup"
+# else
+#     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+# fi
+
 #!/bin/bash
 
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
 
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
-echo "please enter root password:"
-read -s mysql_root_password
+Y="\e[33m"
 
-if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1
-else
-    echo "You are super user."
-fi
-
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then 
-        echo -e "$2...$R FAILURE $N"
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
         exit 1
-    else
-        echo -e "$2...$G SUCCESS $N"
     fi
 }
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
 
-dnf install mysql-server -y &>>$LOGFILE
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
+
+dnf install mysql-server -y &>>$LOG_FILE
 VALIDATE $? "Installing MySQL Server"
 
-systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "Enabling MySQL Server"
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabled MySQL Server"
 
-systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "Starting MySQL Server"
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "Started MySQL server"
 
-# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-# VALIDATE $? "Setting  mysql root password"
-
-# Below code will be useful for idempotent nature
-mysql -h db.soumyadevops.space -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h db.soumyadevops.space -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    VALIDATE $? "MySQL Root password Setup"
+    echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting UP root password"
 else
-    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+    echo -e "MySQL root password is already setup...$Y SKIPPING $N" | tee -a $LOG_FILE
 fi
+
+# Assignment
+# check MySQL Server is installed or not, enabled or not, started or not
+# implement the above things
