16dba89a1b563e9738aa09394dd999fc62393156
soumya <korlagani.soumya@gmail.com>
2025-12-03 14:04:26 +0530

    expense

--- mysql.sh
+++ mysql.sh
@@ -9,6 +9,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+echo "please enter root password:"
+read -s mysql_root_password
 
 if [ $USERID -ne 0 ]
 then
@@ -43,10 +45,10 @@ VALIDATE $? "Starting MySQL Server"
 # VALIDATE $? "Setting  mysql root password"
 
 # Below code will be useful for idempotent nature
-mysql -h db.soumyadevops.space -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+mysql -h db.soumyadevops.space -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ExpenseApp@1
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     VALIDATE $? "MySQL Root password Setup"
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
